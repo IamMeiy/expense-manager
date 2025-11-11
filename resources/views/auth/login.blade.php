aed50ea470e5d468eb16656bbf9f4790dff228c7
Meiy <donmeiyarasan@gmail.com>
2025-11-12 00:14:42 +0530

    Updated the guest layout and register and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,43 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.guest')
 
+@section('title', 'Login Page')
+
+@section('content')
+    <p class="text-center">Sign in to your account</p>
+    @if (session('status'))
+        <div class="alert alert-success mb-3">
+            {{ session('status') }}
+        </div>
+    @endif
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="mb-3">
+            <label for="loginEmail" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" id="loginEmail" aria-describedby="emailHelp">
+            @error('email')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="loginPassword" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="loginPassword">
+            @error('password')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
+        </div>
+        <div class="d-flex align-items-center justify-content-between mb-4">
+            <div class="form-check">
+                <input type="checkbox" name="remember" class="form-check-input" id="rememberMe">
+                <label class="form-check-label" for="rememberMe">Remember Me</label>
+            </div>
+            <a class="text-primary fw-bold" href="{{ route('password.request') }}">Forgot Password ?</a>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+        <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4">Login</button>
+        <div class="d-flex align-items-center justify-content-center">
+            <p class="fs-4 mb-0 fw-bold">Don't have an Account?</p>
+            <a class="text-primary fw-bold ms-2" href="{{ route('register') }}">Sign Up</a>
         </div>
     </form>
-</x-guest-layout>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,44 @@
-<x-guest-layout>
+@extends('layouts.guest')
+
+@section('title', 'Registration Page')
+
+@section('content')
+    <p class="text-center">New Registration</p>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <div class="mb-3">
+            <label for="registerName" class="form-label">Name</label>
+            <input type="text" name="name" class="form-control" id="registerName" aria-describedby="nameHelp">
+            @error('name')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="mb-3">
+            <label for="registerEmail" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" id="registerEmail" aria-describedby="emailHelp">
+            @error('email')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <div class="mb-3">
+            <label for="registerPassword1" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="registerPassword1">
+            @error('password')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
         </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        <div class="mb-3">
+            <label for="registerPassword2" class="form-label">Confirm Password</label>
+            <input type="password" name="password_confirmation" class="form-control" id="registerPassword2">
+            @error('password_confirmation')
+                <p class="text-danger mt-2">{{ $message }}</p>
+            @enderror
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+        <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4">Register</button>
+        <div class="d-flex align-items-center justify-content-center">
+            <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
+            <a class="text-primary fw-bold ms-2" href="{{ route('login') }}">Sign In</a>
         </div>
     </form>
-</x-guest-layout>
+@endsection
