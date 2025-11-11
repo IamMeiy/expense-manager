c07cab33ae46d8d8a48528cad092e53aaf17e62e
Meiy <donmeiyarasan@gmail.com>
2025-11-11 23:05:35 +0530

    Added the master layout and updated the dahboard page

---
+++ resources/views/layouts/aside.blade.php
@@ -0,0 +1,32 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+            <a href="./index.html" class="text-nowrap logo-img">
+                <img src="{{ ASSET_PATH }}template/assets/images/logos/logo-light.svg" alt="" />
+            </a>
+            <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+                <i class="ti ti-x fs-8"></i>
+            </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+            <ul id="sidebarnav">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-6"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link {{ request()->routeIs('dashboard') ? 'active' : '' }}" href="{{ route('dashboard') }}" aria-expanded="false">
+                        <span>
+                            <iconify-icon icon="solar:home-smile-bold-duotone" class="fs-6"></iconify-icon>
+                        </span>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Expense Manager')</title>
+    <link rel="shortcut icon" type="image/png" href="{{ ASSET_PATH }}template/assets/images/logos/seodashlogo.png" />
+    <link rel="stylesheet" href="{{ ASSET_PATH }}template/assets/libs/simplebar/dist/simplebar.min.css">
+    <link rel="stylesheet" href="{{ ASSET_PATH }}template/assets/css/styles.min.css" />
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        <!-- Sidebar Start -->
+        @include('layouts.aside')
+        <!--  Sidebar End -->
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            <!--  Header Start -->
+            <header class="app-header">
+                @include('layouts.nav')
+            </header>
+            <!--  Header End -->
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <script src="{{ ASSET_PATH }}template/assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="{{ ASSET_PATH }}template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ ASSET_PATH }}template/assets/libs/simplebar/dist/simplebar.js"></script>
+    <script src="{{ ASSET_PATH }}template/assets/js/sidebarmenu.js"></script>
+    <script src="{{ ASSET_PATH }}template/assets/js/app.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
